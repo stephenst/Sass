d470fec806d26c11ef39376d781702faf5e604e1
Tony Stephens <tonystephens@gmail.com>
2014-12-18 09:21:14 -0500

    Changed structure of Sass animations. still baselining.

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,17 @@
+/*jslint node:true */
+/*globals pipe */
+
+'use strict';
+
 // Load plugins
 var gulp = require('gulp'),
+    del = require('del'),
     sass = require('gulp-ruby-sass'),
     minifycss = require('gulp-minify-css'),
     rename = require('gulp-rename'),
     notify = require('gulp-notify'),
     jade = require('gulp-jade'),
-    scsslint = require('gulp-scss-lint');
+    scsslint = require('gulp-scss-lint')
 
 
 // SCSS
@@ -13,15 +19,19 @@ gulp.task('styles', function () {
     return gulp.src('src/**/*.scss')
         .pipe(sass({
             sourcemap: true,
-            sourcemapPath: '../scss'
+            sourcemapPath: '',
+            compass: true,
+            style: 'compact'
         }))
         .on('error', function (err) {
             console.log(err.message);
         })
-        .pipe(gulp.dest('./build'))
-        .pipe(notify({
+        .pipe(gulp.dest('build')).pipe(notify({
             message: 'Styles task complete'
         }));
+
+
+
 });
 
 // SCSS Lint
@@ -34,13 +44,36 @@ gulp.task('scsslint', function () {
 
 
 // Jade
-gulp.task('templates', function() {
-    var YOUR_LOCALS = {};
+gulp.task('templates', function () {
+    var YOUR_LOCALS = {
+        basedir: '/github/sass/build/'
+    };
     gulp.src('src/**/*.jade')
         .pipe(jade(({
             locals: YOUR_LOCALS
         })))
         .pipe(gulp.dest('./build/'));
+});
+
+
+/** -----------------------------------------------
+ * Copy
+ * --------------------------------------------- */
+gulp.task('copy', [], function () {
+    gulp.src(['./src/**', '!./src/components/**/*.jade', '!./src/components/**/*.scss'])
+        .pipe(gulp.dest('./build'))
+});
+
+
+/** -----------------------------------------------
+ * Clean
+ * --------------------------------------------- */
+gulp.task('clean', function (callback) {
+    del([
+        './build',
+        // here we use a globbing pattern to match everything inside the `mobile` folder
+        './.sass-cache'
+    ], callback);
 });
 
 // Watch
@@ -49,7 +82,7 @@ gulp.task('watch', function() {
     // Watch .scss files
     gulp.watch('src/**/*.scss', function(event) {
         console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
-        gulp.run('scsslint', 'styles');
+        gulp.run('styles');
     });
 
     // Watch .scss files
@@ -61,4 +94,4 @@ gulp.task('watch', function() {
 
 
 // Default task
-gulp.task('default', ['styles', 'scsslint', 'templates', 'watch']);
+gulp.task('default', ['clean', 'copy', 'styles', 'templates', 'watch']);
